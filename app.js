bf50598ee86bdd096dd2c5f69983ccbc8ed3e58a
Tyler <123tylernguyen@gmail.com>
2022-07-25 18:33:59 -0400

    Created the add food info function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const submitButton = document.querySelector(".submitButton");
-const proteinInput = document.getElementById("protein");
+const proteinInput = document.getElementById('protein');
 const calorieInput = document.getElementById("calorie");
+const mealTimeInput = document.getElementById('mealTime')
+const foodLogContainer = document.querySelector(".foodLog-container");
 
 
 submitButton.addEventListener('click', addinfo);
@@ -9,10 +11,27 @@ submitButton.addEventListener('click', addinfo);
 //functions
 
 function addinfo(e){
+    //default stuff
     e.preventDefault();
-    console.log("yoyo");
-    proteinInput.value = "";
-    calorieInput.value="";
-    
-}
+  
+    //div for new input
+    const timeDiv = document.createElement('div');
+    timeDiv.classList.add('timeDiv');
+    const newInputDiv = document.createElement('div');
+    newInputDiv.classList.add('newInputDiv');
+    console.log(mealTimeInput.value);
 
+    //creating the new input
+    const timeOfMeal = document.createElement('header');
+    timeOfMeal.innerText = mealTimeInput.value;
+    const newInput = document.createElement('li');
+    newInput.innerHTML = proteinInput.value +"g protein, " + calorieInput.value +"cals";
+    timeDiv.appendChild(timeOfMeal);
+    newInputDiv.appendChild(newInput);
+    timeDiv.appendChild(newInputDiv);
+    foodLogContainer.appendChild(timeDiv);
+
+    //reset input text boxes
+    proteinInput.value ="";
+    calorieInput.value="";
+}
