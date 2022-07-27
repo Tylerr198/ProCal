b65d159dd7f97cd28c3c74a32d3a3a2744645fb7
Tyler <123tylernguyen@gmail.com>
2022-07-27 17:56:58 -0400

    page now shows info in local storage

--- app.js
+++ app.js
@@ -13,30 +13,34 @@ submitButton.addEventListener('click', addinfo);
 function addinfo(e){
     //default stuff
     e.preventDefault();
-  
-    //div for new input
-    const timeDiv = document.createElement('div');
-    timeDiv.classList.add('timeDiv');
-    const newInputDiv = document.createElement('div');
-    newInputDiv.classList.add('newInputDiv');
-    console.log(mealTimeInput.value);
+    console.log(proteinInput)
+    if (proteinInput.value === "" || calorieInput.value === ""){
+      return false;
+    } else{
+      //div for new input
+      const timeDiv = document.createElement('div');
+      timeDiv.classList.add('timeDiv');
+      const newInputDiv = document.createElement('div');
+      newInputDiv.classList.add('newInputDiv');
+      console.log(mealTimeInput.value);
 
-    //creating the new input
-    const timeOfMeal = document.createElement('header');
-    timeOfMeal.innerText = mealTimeInput.value;
-    const newInput = document.createElement('li');
-    newInput.innerHTML = proteinInput.value +"g protein, " + calorieInput.value +" calories";
-    timeDiv.appendChild(timeOfMeal);
-    newInputDiv.appendChild(newInput);
-    timeDiv.appendChild(newInputDiv);
-    foodLogContainer.appendChild(timeDiv);
-    saveLocalProtein(proteinInput.value);
-    saveLocalCalorie(calorieInput.value);
-    saveLocalMealtime(mealTimeInput.value);
+      //creating the new input
+      const timeOfMeal = document.createElement('header');
+      timeOfMeal.innerText = mealTimeInput.value;
+      const newInput = document.createElement('li');
+      newInput.innerHTML = proteinInput.value +"g protein, " + calorieInput.value +" calories";
+      timeDiv.appendChild(timeOfMeal);
+      newInputDiv.appendChild(newInput);
+      timeDiv.appendChild(newInputDiv);
+      foodLogContainer.appendChild(timeDiv);
+      saveLocalProtein(proteinInput.value);
+      saveLocalCalorie(calorieInput.value);
+      saveLocalMealtime(mealTimeInput.value);
 
-    //reset input text boxes
-    proteinInput.value ="";
-    calorieInput.value="";
+      //reset input text boxes
+      proteinInput.value ="";
+      calorieInput.value="";
+    }
 }
 
 function saveLocalProtein(proteinInfo) {
@@ -78,26 +82,30 @@ function saveLocalProtein(proteinInfo) {
         protein = [];
       } else {
         protein = JSON.parse(localStorage.getItem("protein"));
+        calorie = JSON.parse(localStorage.getItem("calorie"));
+        mealTimes = JSON.parse(localStorage.getItem("mealTimes"));
       }
-    const timeDiv = document.createElement('div');
-    timeDiv.classList.add('timeDiv');
-    const newInputDiv = document.createElement('div');
-    newInputDiv.classList.add('newInputDiv');
-    console.log(mealTimeInput.value);
+    for (let i = 0; i<protein.length; i++){
+      const timeDiv = document.createElement('div');
+      timeDiv.classList.add('timeDiv');
+      const newInputDiv = document.createElement('div');
+      newInputDiv.classList.add('newInputDiv');
+      
 
-    //creating the new input
-    const timeOfMeal = document.createElement('header');
-    timeOfMeal.innerText = mealTimeInput.value;
-    const newInput = document.createElement('li');
-    newInput.innerHTML = proteinInput.value +"g protein, " + calorieInput.value +" calories";
-    timeDiv.appendChild(timeOfMeal);
-    newInputDiv.appendChild(newInput);
-    timeDiv.appendChild(newInputDiv);
-    foodLogContainer.appendChild(timeDiv);
-    saveLocalProtein(proteinInput.value);
-    saveLocalCalorie(calorieInput.value);
-    saveLocalMealtime(mealTimeInput.value);
+      //creating the new input
+      const timeOfMeal = document.createElement('header');
+      timeOfMeal.innerText = mealTimes[i];
+      const newInput = document.createElement('li');
+      newInput.innerHTML = protein[i] +"g protein, " + calorie[i] +" calories";
+      timeDiv.appendChild(timeOfMeal);
+      newInputDiv.appendChild(newInput);
+      timeDiv.appendChild(newInputDiv);
+      foodLogContainer.appendChild(timeDiv);
+    }
   }
+
+getInfo();
+
 
 
 
